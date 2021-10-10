665f2aa95cc8398747afd26b5c9ffb077b658077
Kimhyelyn <44598936+merry-hyelyn@users.noreply.github.com>
2021-10-10 12:24:24 +0900

    week8 answer

--- Week8/answer.py
+++ Week8/answer.py
@@ -1,25 +1,7 @@
-def check_turn_size(w, h, card_w, card_h):
-    if card_w <= w and card_h <= h:
-        return True
-    return False
-
-
 def solution(sizes):
-    answer = 0
-    width, height = zip(* sizes)
-    max_card = max([k * v for k, v in sizes])
-    wallets = [(w, h) for w in width for h in height if w * h >= max_card]
-    for w, h in sorted(wallets, reverse=True):
-        count = 0
-        for size in sizes:
-            card_w, card_h = size
-            if card_w > w or card_h > h:
-                if not check_turn_size(h, w, card_w, card_h):
-                    count += 1
-        if count == 0:
-            answer = w * h
-    return answer
-# size 케이스가 모두 통과해야함!
+    cards = [(max(size), min(size)) for size in sizes]
+    width, height = zip(*cards)
+    return max(width) * max(height)
 
 
 print("answer : ", solution([[60, 50], [30, 70], [60, 30], [80, 40]]))
